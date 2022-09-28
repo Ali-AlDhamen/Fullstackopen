6b52bc80aac358a20c05069ebad2d2f43f2e3925
Pali2002 <93004494+Pali2002@users.noreply.github.com>
2022-09-28 21:04:31 +0300

    Delete part2/phonebook directory

--- part2/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ onChange, value }) => {
-  return (
-    <div>
-      Search with name <input onChange={onChange} value={value} />
-    </div>
-  );
-};
-
-export default Filter;
--- part2/phonebook/src/components/Text.js
+++
@@ -1,4 +0,0 @@
-const Text = ({ text }) => {
-  return <h2>{text}</h2>;
-};
-export default Text;
